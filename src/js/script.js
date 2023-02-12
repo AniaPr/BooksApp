46e04d853958fbf392958a158c57f37ebf6d7128
Anna Prusińska <anna.prusinska12@gmail.com>
2023-02-12 15:27:54 +0100

    add function initActions

--- src/js/script.js
+++ src/js/script.js
@@ -14,9 +14,35 @@ function render() {
     const generateDOM = utils.createDOMFromHTML(generateHTML);
 
     const domElement = document.querySelector('.books-list');
-
+    //console.log(domElement);
     domElement.appendChild(generateDOM);
   }
 }
 
+function initActions() {
+  const bookImages = document.querySelectorAll('.book__image');
+  const favoriteBooks = [];
+
+  for (let bookImage of bookImages) {
+    bookImage.addEventListener('dblclick', function (event) {
+      event.preventDefault;
+
+      const bookId = bookImage.getAttribute('data-id');
+      if (favoriteBooks.indexOf(bookId) == -1) {
+        bookImage.classList.add('favorite');
+
+        favoriteBooks.push(bookId);
+        //console.log(favoriteBooks);
+      } else {
+        bookImage.classList.remove('favorite');
+
+        const indexOfBook = favoriteBooks.indexOf(bookId);
+        favoriteBooks.splice(indexOfBook, 1);
+      }
+      console.log('FavoriteBooks:', favoriteBooks);
+    });
+  }
+}
+
 render();
+initActions();
