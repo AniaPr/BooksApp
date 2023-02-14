0bb063df711f8e881515856f92342ea303b7afb1
Anna Prusińska <anna.prusinska12@gmail.com>
2023-02-14 02:58:24 +0100

    add function filterBooks

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ const templates = {
   ),
 };
 
+const filters = [];
+
 function render() {
   const booksList = dataSource.books;
   for (let book in booksList) {
@@ -20,12 +22,13 @@ function render() {
 
 function initActions() {
   const domElement = document.querySelector('.books-list');
-  console.log(domElement);
   const favoriteBooks = [];
 
-  domElement.addEventListener('dblclick', function (event) {
+  domElement.addEventListener('click', function (event) {
     event.preventDefault();
+  });
 
+  domElement.addEventListener('dblclick', function (event) {
     const img = event.target.offsetParent;
     const bookId = img.getAttribute('data-id');
 
@@ -39,6 +42,52 @@ function initActions() {
     }
     console.log('FavoriteBooks:', favoriteBooks);
   });
+
+  const filtersForm = document.querySelector('.filters');
+
+  filtersForm.addEventListener('click', function (event) {
+    console.log('Checkbox was clicked!');
+    if (
+      event.target.tagName == 'INPUT' &&
+      event.target.type == 'checkbox' &&
+      event.target.name == 'filter'
+    ) {
+      console.log('value:', event.target.value);
+
+      if (event.target.checked == true) {
+        filters.push(event.target.value);
+      } else {
+        const indexOfFilter = filters.indexOf(event.target.value);
+        filters.splice(indexOfFilter, 1);
+      }
+
+      console.log(filters);
+    }
+    filterBooks();
+  });
+}
+
+function filterBooks() {
+  for (let book of dataSource.books) {
+    let shouldBeHidden = false;
+    const filterElem = document.querySelector(
+      `.book__image[data-id="${book.id}"]`
+    );
+
+    for (const filter of filters) {
+      if (book.details[filter]) {
+        shouldBeHidden = true;
+        break;
+      }
+      console.log(filterElem, book.details[filter]);
+    }
+
+    if (shouldBeHidden) {
+      filterElem.classList.add('hidden');
+    } else {
+      filterElem.classList.remove('hidden');
+    }
+  }
 }
 
 render();
